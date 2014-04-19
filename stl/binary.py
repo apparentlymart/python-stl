238b45450b1a827c0965200966382ae01e6bb6a1
Martin Atkins <mart@degeneration.co.uk>
2014-04-19 15:03:57 -0700

    Parsing for Binary STL.

--- stl/binary.py
+++ stl/binary.py
@@ -1,6 +1,70 @@
 
+import struct
 from stl.types import *
 
 
+class Reader(object):
+
+    def __init__(self, file):
+        self.file = file
+        self.offset = 0
+
+    def read_bytes(self, byte_count):
+        bytes = self.file.read(byte_count)
+        if len(bytes) < byte_count:
+            raise FormatError(
+                "Unexpected end of file at offset %i" % (
+                    self.offset + len(bytes),
+                )
+            )
+        self.offset += byte_count
+        return bytes
+
+    def read_uint32(self):
+        bytes = self.read_bytes(4)
+        return struct.unpack('<I', bytes)[0]
+
+    def read_uint16(self):
+        bytes = self.read_bytes(2)
+        return struct.unpack('<H', bytes)[0]
+
+    def read_float(self):
+        bytes = self.read_bytes(4)
+        return struct.unpack('<f', bytes)[0]
+
+    def read_vector3d(self):
+        x = self.read_float()
+        y = self.read_float()
+        z = self.read_float()
+        return Vector3d(x, y, z)
+
+
+class FormatError(ValueError):
+    pass
+
+
 def parse(file):
-    return None
+    r = Reader(file)
+
+    ret = Solid()
+
+    # Skip the header
+    r.read_bytes(80)
+
+    num_facets = r.read_uint32()
+
+    for i in xrange(0, num_facets):
+        normal = r.read_vector3d()
+        vertices = tuple(
+            r.read_vector3d() for j in xrange(0, 3)
+        )
+        ret.add_facet(
+            normal=normal,
+            vertices=vertices,
+        )
+        attr_byte_count = r.read_uint16()
+        if attr_byte_count > 0:
+            # skip attribute bytes
+            r.read_bytes(attr_byte_count)
+
+    return ret
---
+++ tests/test_binary.py
@@ -0,0 +1,96 @@
+
+import unittest
+from stl.binary import *
+
+
+EMPTY_HEADER = '\0' * 80
+
+
+class TestParser(unittest.TestCase):
+
+    def _parse_str(self, string):
+        from StringIO import StringIO
+        return parse(StringIO(string))
+
+    def test_empty(self):
+        with self.assertRaises(FormatError):
+            self._parse_str('')
+
+    def test_no_facets(self):
+        solid = self._parse_str(
+            EMPTY_HEADER + '\0\0\0\0'
+        )
+        self.assertEqual(
+            solid,
+            Solid(
+                name=None,
+                facets=[],
+            ),
+        )
+
+    def test_missing_facets(self):
+        with self.assertRaises(FormatError):
+            # Declared that we have two facets but we
+            # actually have none.
+            self._parse_str(
+                EMPTY_HEADER + '\x02\x00\x00\x00'
+            )
+
+    def test_valid(self):
+        solid = self._parse_str(
+            EMPTY_HEADER +
+            '\x02\x00\x00\x00' # two facets
+            # first facet
+            '\x00\x00\x80\x3f' # normal x = 1.0
+            '\x00\x00\x00\x40' # normal y = 2.0
+            '\x00\x00\x40\x40' # normal z = 3.0
+            '\x00\x00\x80\x40' # vertex x = 4.0
+            '\x00\x00\xa0\x40' # vertex y = 5.0
+            '\x00\x00\xc0\x40' # vertex z = 6.0
+            '\x00\x00\xe0\x40' # vertex x = 7.0
+            '\x00\x00\x00\x41' # vertex y = 8.0
+            '\x00\x00\x10\x41' # vertex z = 9.0
+            '\x00\x00\x20\x41' # vertex x = 10.0
+            '\x00\x00\x30\x41' # vertex y = 11.0
+            '\x00\x00\x40\x41' # vertex z = 12.0
+            '\x04\x00'         # four attribute bytes
+            '\x00\x00\x80\x7f' # dummy attribute bytes (float Infinity)
+            # second facet
+            '\x00\x00\x80\x3f' # normal x = 1.0
+            '\x00\x00\x80\x3f' # normal y = 1.0
+            '\x00\x00\x80\x3f' # normal z = 1.0
+            '\x00\x00\x80\x3f' # vertex x = 1.0
+            '\x00\x00\x80\x3f' # vertex y = 1.0
+            '\x00\x00\x80\x3f' # vertex z = 1.0
+            '\x00\x00\x80\x3f' # vertex x = 1.0
+            '\x00\x00\x80\x3f' # vertex y = 1.0
+            '\x00\x00\x80\x3f' # vertex z = 1.0
+            '\x00\x00\x80\x3f' # vertex x = 1.0
+            '\x00\x00\x80\x3f' # vertex y = 1.0
+            '\x00\x00\x80\x3f' # vertex z = 1.0
+            '\x00\x00'         # no attribute bytes
+        )
+        self.assertEqual(
+            solid,
+            Solid(
+                name=None,
+                facets=[
+                    Facet(
+                        normal=Vector3d(1.0, 2.0, 3.0),
+                        vertices=(
+                            Vector3d(4.0, 5.0, 6.0),
+                            Vector3d(7.0, 8.0, 9.0),
+                            Vector3d(10.0, 11.0, 12.0),
+                        ),
+                    ),
+                    Facet(
+                        normal=Vector3d(1.0, 1.0, 1.0),
+                        vertices=(
+                            Vector3d(1.0, 1.0, 1.0),
+                            Vector3d(1.0, 1.0, 1.0),
+                            Vector3d(1.0, 1.0, 1.0),
+                        ),
+                    ),
+                ],
+            ),
+        )
